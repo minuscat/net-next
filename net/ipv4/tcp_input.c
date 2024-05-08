30be3958ebcb5d632b335147f4a8f1cfb0853893
Ilpo Järvinen <ij@kernel.org>
2024-05-08 04:03:13 +0200

    tcp: accecn: add AccECN rx byte counters
    These three byte counters track IP ECN field payload byte sums for
    all arriving (acceptable) packets for ECT0, ECT1, and CE. The
    AccECN option (added by a later patch in the series) echoes these
    counters back to sender side; therefore, it is placed within the
    group of tcp_sock_write_txrx.
    
    Below are the pahole outcomes before and after this patch, in which
    the group size of tcp_sock_write_txrx is increased from 95 + 4 to
    107 + 4 and an extra 4-byte hole is created but will be exploited
    in later patches:
    
    [BEFORE THIS PATCH]
    struct tcp_sock {
        [...]
        u32                        delivered_ce;         /*  2640     4 */
        u32                        received_ce;          /*  2644     4 */
        u32                        app_limited;          /*  2648     4 */
        u32                        rcv_wnd;              /*  2652     4 */
        struct tcp_options_received rx_opt;              /*  2656    24 */
        __cacheline_group_end__tcp_sock_write_txrx[0];   /*  2680     0 */
    
        [...]
        /* size: 3264, cachelines: 51, members: 169 */
    }
    
    [AFTER THIS PATCH]
    struct tcp_sock {
        [...]
        u32                        delivered_ce;         /*  2640     4 */
        u32                        received_ce;          /*  2644     4 */
        u32                        received_ecn_bytes[3];/*  2648    12 */
        u32                        app_limited;          /*  2660     4 */
        u32                        rcv_wnd;              /*  2664     4 */
        struct tcp_options_received rx_opt;              /*  2668    24 */
        __cacheline_group_end__tcp_sock_write_txrx[0];   /*  2692     0 */
        /* XXX 4 bytes hole, try to pack */
    
        [...]
        /* size: 3264, cachelines: 51, members: 170 */
    }
    
    Signed-off-by: Ilpo Järvinen <ij@kernel.org>
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Co-developed-by: Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>
    Signed-off-by: Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>
    
    ---
    v8:
    - Add new helper function tcp_ecn_received_counters_payload()

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -6013,8 +6013,17 @@ static void tcp_urg(struct sock *sk, struct sk_buff *skb, const struct tcphdr *t
 	}
 }
 
+void tcp_ecn_received_counters_payload(struct sock *sk,
+				       const struct sk_buff *skb)
+{
+	const struct tcphdr *th = (const struct tcphdr *)skb->data;
+
+	tcp_ecn_received_counters(sk, skb, skb->len - th->doff * 4);
+}
+
 /* Updates Accurate ECN received counters from the received IP ECN field */
-void tcp_ecn_received_counters(struct sock *sk, const struct sk_buff *skb)
+void tcp_ecn_received_counters(struct sock *sk, const struct sk_buff *skb,
+			       u32 payload_len)
 {
 	u8 ecnfield = TCP_SKB_CB(skb)->ip_dsfield & INET_ECN_MASK;
 	u8 is_ce = INET_ECN_is_ce(ecnfield);
@@ -6034,6 +6043,9 @@ void tcp_ecn_received_counters(struct sock *sk, const struct sk_buff *skb)
 		tp->received_ce += pcount;
 		tp->received_ce_pending = min(tp->received_ce_pending + pcount,
 					      0xfU);
+
+		if (payload_len > 0)
+			tp->received_ecn_bytes[ecnfield - 1] += payload_len;
 	}
 }
 
@@ -6307,7 +6319,7 @@ void tcp_rcv_established(struct sock *sk, struct sk_buff *skb)
 					flag |= __tcp_replace_ts_recent(tp,
 									delta);
 
-				tcp_ecn_received_counters(sk, skb);
+				tcp_ecn_received_counters(sk, skb, 0);
 
 				/* We know that such packets are checksummed
 				 * on entry.
@@ -6353,7 +6365,8 @@ void tcp_rcv_established(struct sock *sk, struct sk_buff *skb)
 			/* Bulk data transfer: receiver */
 			tcp_cleanup_skb(skb);
 			__skb_pull(skb, tcp_header_len);
-			tcp_ecn_received_counters(sk, skb);
+			tcp_ecn_received_counters(sk, skb,
+						  len - tcp_header_len);
 			eaten = tcp_queue_rcv(sk, skb, &fragstolen);
 
 			tcp_event_data_recv(sk, skb);
@@ -6400,7 +6413,7 @@ step5:
 			tcp_accecn_third_ack(sk, skb, tp->syn_ect_snt);
 		tcp_fast_path_on(tp);
 	}
-	tcp_ecn_received_counters(sk, skb);
+	tcp_ecn_received_counters_payload(sk, skb);
 
 	reason = tcp_ack(sk, skb, FLAG_SLOWPATH | FLAG_UPDATE_TS_RECENT);
 	if ((int)reason < 0) {
