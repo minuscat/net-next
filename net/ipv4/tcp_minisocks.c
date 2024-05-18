151ca17b66a441d942b35f8f3a08d8540174d5b7
Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>
2024-05-18 04:32:46 +0200

    tcp: accecn: unset ECT if receive or send ACE=0 in AccECN negotiaion
    Based on specification:
      https://tools.ietf.org/id/draft-ietf-tcpm-accurate-ecn-28.txt
    
    Based on Section 3.1.5 of AccECN spec (RFC9768), a TCP Server in
    AccECN mode MUST NOT set ECT on any packet for the rest of the connection,
    if it has received or sent at least one valid SYN or Acceptable SYN/ACK
    with (AE,CWR,ECE) = (0,0,0) during the handshake.
    
    In addition, a host in AccECN mode that is feeding back the IP-ECN
    field on a SYN or SYN/ACK MUST feed back the IP-ECN field on the
    latest valid SYN or acceptable SYN/ACK to arrive.
    
    Signed-off-by: Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    
    ---
    v10:
    - Set AccECN error flag on request itself via tcp_rsk() to avoid
      changing directly on the listener
    
    v8:
    - Add new helper function tcp_accecn_ace_fail_send_set_retrans()
    
    v6:
    - Do not cast const struct request_sock into struct request_sock
    - Set tcp_accecn_fail_mode after calling tcp_rtx_synack().

--- net/ipv4/tcp_minisocks.c
+++ net/ipv4/tcp_minisocks.c
@@ -481,6 +481,8 @@ static void tcp_ecn_openreq_child(struct sock *sk,
 		tp->syn_ect_snt = treq->syn_ect_snt;
 		tcp_accecn_third_ack(sk, skb, treq->syn_ect_snt);
 		tp->saw_accecn_opt = treq->saw_accecn_opt;
+		if (treq->accecn_fail_mode & TCP_ACCECN_ACE_FAIL_SEND)
+			tcp_accecn_fail_mode_set(tp, TCP_ACCECN_ACE_FAIL_SEND);
 		tp->prev_ecnfield = treq->syn_ect_rcv;
 		tp->accecn_opt_demand = 1;
 		tcp_ecn_received_counters_payload(sk, skb);
@@ -749,16 +751,32 @@ struct sock *tcp_check_req(struct sock *sk, struct sk_buff *skb,
 		 */
 		if (!tcp_oow_rate_limited(sock_net(sk), skb,
 					  LINUX_MIB_TCPACKSKIPPEDSYNRECV,
-					  &tcp_rsk(req)->last_oow_ack_time) &&
+					  &tcp_rsk(req)->last_oow_ack_time)) {
+			if (tcp_rsk(req)->accecn_ok) {
+				u8 ect_rcv = TCP_SKB_CB(skb)->ip_dsfield &
+					     INET_ECN_MASK;
 
-		    !tcp_rtx_synack(sk, req)) {
-			unsigned long expires = jiffies;
+				tcp_rsk(req)->syn_ect_rcv = ect_rcv;
+				if (tcp_accecn_ace(tcp_hdr(skb)) == 0x0) {
+					u8 fail_mode = TCP_ACCECN_ACE_FAIL_RECV;
 
-			expires += tcp_reqsk_timeout(req);
-			if (!fastopen)
-				mod_timer_pending(&req->rsk_timer, expires);
-			else
-				req->rsk_timer.expires = expires;
+					tcp_accecn_fail_mode_set(tcp_sk(sk),
+								 fail_mode);
+				}
+			}
+			if (!tcp_rtx_synack(sk, req)) {
+				unsigned long expires = jiffies;
+
+				tcp_accecn_ace_fail_send_set_retrans(req,
+								     tcp_sk(sk));
+
+				expires += tcp_reqsk_timeout(req);
+				if (!fastopen)
+					mod_timer_pending(&req->rsk_timer,
+							  expires);
+				else
+					req->rsk_timer.expires = expires;
+			}
 		}
 		return NULL;
 	}
