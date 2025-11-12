9b2c2a8f1d36fa9236f9e1e462c5875cd26411c0
Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>
2025-11-12 11:53:50 +0100

    tcp: move increment of num_retrans
    Before this patch, num_retrans = 0 for the first SYN/ACK and the first
    retransmitted SYN/ACK; however, an upcoming change will need to
    differentiate between those two conditions. This patch moves the
    increment of num_tranns before rtx_syn_ack() so we can distinguish
    between these two cases when making SYN/ACK.
    
    Signed-off-by: Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -4607,6 +4607,7 @@ int tcp_rtx_synack(const struct sock *sk, struct request_sock *req)
 	/* Paired with WRITE_ONCE() in sock_setsockopt() */
 	if (READ_ONCE(sk->sk_txrehash) == SOCK_TXREHASH_ENABLED)
 		WRITE_ONCE(tcp_rsk(req)->txhash, net_tx_rndhash());
+	WRITE_ONCE(req->num_retrans, req->num_retrans + 1);
 	res = af_ops->send_synack(sk, NULL, &fl, req, NULL, TCP_SYNACK_NORMAL,
 				  NULL);
 	if (!res) {
@@ -4620,7 +4621,8 @@ int tcp_rtx_synack(const struct sock *sk, struct request_sock *req)
 			tcp_sk_rw(sk)->total_retrans++;
 		}
 		trace_tcp_retransmit_synack(sk, req);
-		WRITE_ONCE(req->num_retrans, req->num_retrans + 1);
+	} else {
+		WRITE_ONCE(req->num_retrans, req->num_retrans - 1);
 	}
 	return res;
 }
